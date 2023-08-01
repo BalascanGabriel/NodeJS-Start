020e6ba9a4e71530d6bce1d9dd52fdd6a1d9d248
Balascan Gabriel-Danut <balascangabriel@yahoo.com>
2023-08-01 22:25:31 +0300

    First 5 mins into

--- starter/index.js
+++ starter/index.js
@@ -3,5 +3,8 @@ const fs = require('fs');
 
 const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
 // read the content of input.txt and return it as string in utf-8 encoding
-
 console.log(textIn);
+
+const textOut = `this is the text from another file  : ${textIn}. \nCreated on ${Date.now()}`;
+fs.writeFileSync('./txt/output.txt',textOut);
+console.log('Written suceesfully !');
