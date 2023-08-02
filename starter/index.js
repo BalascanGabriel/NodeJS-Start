3902b10687b6d6692bf1a20d0c350700775fea30
Balascan Gabriel-Danut <balascangabriel@yahoo.com>
2023-08-02 10:05:12 +0300

    Dummy routing

--- starter/index.js
+++ starter/index.js
@@ -1,6 +1,7 @@
 ///require fs which stands for file-system -> fs is a module
 const fs = require('fs');
-
+const http = require('http');
+const url = require('url');
 ////////////////
 //FILES
 
@@ -39,3 +40,41 @@ const fs = require('fs');
 
 ////////////////////
 //SERVER
+
+const data = fs.readFileSync('./dev-data/data.json', 'utf-8');
+const productData = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+    const pathName = req.url;
+
+    if (pathName === '/') {
+        res.write('Response from the server !\n');
+        res.end('Hello from the server !');
+    } else if (pathName === '/overview') {
+        res.write('Response from the server !\n');
+        res.end('This is the OVERVIEW!');
+    } else if (pathName === '/product') {
+        res.write('Response from the server !\n');
+        res.end('This is the PRODUCT !');
+    } else if (pathName === '/api') { 
+
+    res.writeHead(200, {
+        'Content-type': 'application/json'
+    })
+    res.end(data);
+
+}
+        else {
+    res.writeHead(404, {
+        'Content-type': 'text/html',
+        'my-own-header': 'hello-world'
+    });
+    res.end('<h1>PAGE NOT FOUND !!!</h1>');
+}
+});
+
+const port = 8000;
+
+server.listen(port, '127.0.0.1', () => {
+    console.log(`Listening on port ${port}`);
+});
