24e19055bcaf98504dce261c742fa35e71c9f9d5
Balascan Gabriel-Danut <balascangabriel@yahoo.com>
2023-08-02 08:47:09 +0300

    Asynchronous file read

--- starter/index.js
+++ starter/index.js
@@ -1,10 +1,41 @@
 ///require fs which stands for file-system -> fs is a module
 const fs = require('fs');
 
-const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
-// read the content of input.txt and return it as string in utf-8 encoding
-console.log(textIn);
+////////////////
+//FILES
 
-const textOut = `this is the text from another file  : ${textIn}. \nCreated on ${Date.now()}`;
-fs.writeFileSync('./txt/output.txt',textOut);
-console.log('Written suceesfully !');
+// //Blocking / Synchronous way
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+// // read the content of input.txt and return it as string in utf-8 encoding
+// console.log(textIn);
+
+// const textOut = `this is the text from another file  : ${textIn}. \nCreated on ${Date.now()}`;
+// fs.writeFileSync('./txt/output.txt',textOut);
+// console.log('Written suceesfully !');
+
+//Nonblocking/ Asynchronous way
+// try {
+//     fs.readFile('./txt/start.txt', 'utf-8', (err, data) => {
+//         if (err) throw err;
+//         console.log(data + '\n');
+//         fs.readFile('./txt/input.txt', 'utf-8', (err, data2) => {
+//             if (err) throw err;
+//             console.log(data2 + '\n');
+
+//             console.log("\nData read with success !");
+
+//             fs.writeFile('./txt/last.txt', `${data}\n${data2}`, 'utf-8', err => {
+//                 if (err) throw err;
+//                 console.log('Files written with success !');
+//             });
+//         });
+//     });
+// } catch (error) {
+//     console.log('An error occurred: ', error.message);
+// }
+
+// console.log("Will read file !!!");
+
+
+////////////////////
+//SERVER
